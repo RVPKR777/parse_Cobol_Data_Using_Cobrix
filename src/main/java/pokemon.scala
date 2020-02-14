d568821403d1b8dfbcf74772ac77ed7172bcb44d
RVPKR777 <pavan.03121996@gmail.com>
2020-02-14 09:28:53 -0500

    latest update

--- src/main/java/pokemon.scala
+++ src/main/java/pokemon.scala
@@ -2,11 +2,14 @@ import org.apache.spark.sql.SparkSession
 
 object pokemon {
 
+
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().config("spark.master", "local").appName("Pokemon").getOrCreate()
-    val filerdd = spark.read.textFile("C:/Users/pavan/Desktop/pokemon.csv").cache()
-    val namerdd = filerdd.filter(line => line.contains("water")).count()
-    println(s"$namerdd")
+    val filerdd = spark.read.textFile("C:/Users/pavan/Desktop/pokemon.csv")
+    //val namerdd = filerdd.filter(line => line.contains("water")).count()
+    //println(filerdd.first())
+     // println(filerdd.map(_.split(",")(2)))
+    val a = filerdd.map(_.split(",")(2))
     spark.stop()
   }
 }
